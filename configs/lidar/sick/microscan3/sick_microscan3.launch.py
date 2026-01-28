2838a8f5183589b7854c2ff9b162c4e5f5acafe1
Robert Fischer <robertfischer654@gmail.com>
2026-01-28 13:27:27 +0000

    namespacing test

--- configs/lidar/sick/microscan3/sick_microscan3.launch.py
+++ configs/lidar/sick/microscan3/sick_microscan3.launch.py
@@ -22,63 +22,66 @@ def generate_launch_description():
     
     return LaunchDescription([
         DeclareLaunchArgument('namespace', default_value=''),
-        Node(
-            package="sick_safetyscanners2",
-            executable="sick_safetyscanners2_node",
-            name="sick_safetyscanners2_node",
-            output="screen",
-            emulate_tty=True,
-            parameters=[
-                {"frame_id": frame_id_1,
-                 "sensor_ip": "192.168.1.30",
-                 "host_ip": "192.168.1.10",
-                 "interface_ip": "0.0.0.0",
-                 "host_udp_port": 6060,
-                 "channel": 0,
-                 "channel_enabled": True,
-                 "skip": 1,
-                 "angle_start": -1.48,
-                 "angle_end": 1.48,
-                 "time_offset": -0.07,
-                 "general_system_state": True,
-                 "derived_settings": True,
-                 "measurement_data": True,
-                 "intrusion_data": True,
-                 "application_io_data": True,
-                 "use_persistent_config": False,
-                 "min_intensities": 0.0}
-            ],
-            remappings=[
-                ('scan', 'lidar_1/scan_filtered'),
-            ]
-        ), Node(
-            package="sick_safetyscanners2",
-            executable="sick_safetyscanners2_node",
-            name="sick_safetyscanners2_node1",
-            output="screen",
-            emulate_tty=True,
-            parameters=[
-                {"frame_id": frame_id_2,
-                 "sensor_ip": "192.168.1.31",
-                 "host_ip": "192.168.1.10",
-                 "interface_ip": "0.0.0.0",
-                 "host_udp_port": 6061,
-                 "channel": 0,
-                 "channel_enabled": True,
-                 "skip": 1,
-                 "angle_start": -1.48,
-                 "angle_end": 1.48,
-                 "time_offset": -0.07,
-                 "general_system_state": True,
-                 "derived_settings": True,
-                 "measurement_data": True,
-                 "intrusion_data": True,
-                 "application_io_data": True,
-                 "use_persistent_config": False,
-                 "min_intensities": 0.0}
-            ],
-            remappings=[
-                ('scan', 'lidar_2/scan_filtered'),
-            ]
-        )
+        GroupAction([
+            PushRosNamespace(namespace),
+            Node(
+                package="sick_safetyscanners2",
+                executable="sick_safetyscanners2_node",
+                name="sick_safetyscanners2_node",
+                output="screen",
+                emulate_tty=True,
+                parameters=[
+                    {"frame_id": frame_id_1,
+                    "sensor_ip": "192.168.1.30",
+                    "host_ip": "192.168.1.10",
+                    "interface_ip": "0.0.0.0",
+                    "host_udp_port": 6060,
+                    "channel": 0,
+                    "channel_enabled": True,
+                    "skip": 1,
+                    "angle_start": -1.48,
+                    "angle_end": 1.48,
+                    "time_offset": -0.07,
+                    "general_system_state": True,
+                    "derived_settings": True,
+                    "measurement_data": True,
+                    "intrusion_data": True,
+                    "application_io_data": True,
+                    "use_persistent_config": False,
+                    "min_intensities": 0.0}
+                ],
+                remappings=[
+                    ('scan', 'lidar_1/scan_filtered'),
+                ]
+            ), Node(
+                package="sick_safetyscanners2",
+                executable="sick_safetyscanners2_node",
+                name="sick_safetyscanners2_node1",
+                output="screen",
+                emulate_tty=True,
+                parameters=[
+                    {"frame_id": frame_id_2,
+                    "sensor_ip": "192.168.1.31",
+                    "host_ip": "192.168.1.10",
+                    "interface_ip": "0.0.0.0",
+                    "host_udp_port": 6061,
+                    "channel": 0,
+                    "channel_enabled": True,
+                    "skip": 1,
+                    "angle_start": -1.48,
+                    "angle_end": 1.48,
+                    "time_offset": -0.07,
+                    "general_system_state": True,
+                    "derived_settings": True,
+                    "measurement_data": True,
+                    "intrusion_data": True,
+                    "application_io_data": True,
+                    "use_persistent_config": False,
+                    "min_intensities": 0.0}
+                ],
+                remappings=[
+                    ('scan', 'lidar_2/scan_filtered'),
+                ]
+            )
+        ])
     ])
